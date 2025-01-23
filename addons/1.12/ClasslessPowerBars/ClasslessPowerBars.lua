8be9403904a111504d58dd63d34138a13e582797
David <davidparraausina@gmail.com>
2025-01-23 20:40:43 +0000

    Added player power regeneration update and addon message

--- addons/1.12/ClasslessPowerBars/ClasslessPowerBars.lua
+++ addons/1.12/ClasslessPowerBars/ClasslessPowerBars.lua
@@ -1,128 +1,38 @@
---local _, playerClass = UnitClass("player")
---if playerClass ~= "DRUID" then return end
+ClasslessPowerBarsDB = {}
 
-local ClasslessPowerBars = AceLibrary("AceAddon-2.0"):new("AceConsole-2.0", "AceEvent-2.0", "AceDB-2.0")
-local DruidManaLib = AceLibrary("DruidManaLib-1.0")
-local L = AceLibrary("AceLocale-2.2"):new("ClasslessPowerBars")
+ClasslessPowerBars = CreateFrame("Frame")
 
-local TEXTURE = "Interface\\TargetingFrame\\UI-StatusBar"
+ClasslessPowerBars.initialized = false
+ClasslessPowerBars.debug = true
+ClasslessPowerBars.serverMessagePrefix = "classless"
 
-function ClasslessPowerBars:OnInitialize()
-	self:RegisterDB("ClasslessPowerBarsDB")
-	self:RegisterDefaults("profile", { moveAbove = false })
-	self:RegisterChatCommand({ "/classlesspowerbars", "/cpb" }, {
-		type = "group",
-		args = {
-			moveabove = {
-				type = "toggle",
-				name = L["Move Above"],
-				desc = L["Move the bar above the player frame."],
-				get = "IsMoveAbove",
-				set = "ToggleMoveAbove",
-			},
-		},
-	})
-end
+ClasslessPowerBars:RegisterEvent("CHAT_MSG_ADDON")
+ClasslessPowerBars:RegisterEvent("ADDON_LOADED")
+ClasslessPowerBars:RegisterEvent("PLAYER_ENTERING_WORLD")
 
-function ClasslessPowerBars:OnEnable()
-	DEFAULT_CHAT_FRAME:AddMessage("ClasslessPowerBars:OnEnable")
-
-	if not self.bar then
-		DEFAULT_CHAT_FRAME:AddMessage("ClasslessPowerBars:OnEnable create bar")
-		local bar = CreateFrame("StatusBar", "ClasslessPowerBars", PlayerFrame, "TextStatusBar")
-		bar:SetWidth(78)
-		bar:SetHeight(12)
-		bar:SetStatusBarTexture(TEXTURE)
-		bar:SetStatusBarColor(ManaBarColor[0].r, ManaBarColor[0].g, ManaBarColor[0].b)
-
-		local bg = bar:CreateTexture("$parentBackground", "BACKGROUND")
-		bg:SetAllPoints(bar)
-		bg:SetTexture(TEXTURE)
-		bg:SetVertexColor(0, 0, 0, 0.5)
-		bar.bg = bg
-
-		local bd = bar:CreateTexture("$parentBorder", "OVERLAY")
-		bd:SetWidth(97)
-		bd:SetHeight(16)
-		bd:SetTexture("Interface\\CharacterFrame\\UI-CharacterFrame-GroupIndicator")
-		bar.bd = bd
-
-		local text = bar:CreateFontString("$parentText", "OVERLAY", "TextStatusBarText")
-		text:SetPoint("CENTER", 0, 0)
-		SetTextStatusBarText(bar, text)
-		bar.textLockable = 1
-		bar.text = text
-		
-		-- Enable dragging
-		bar:SetMovable(true)
-		bar:EnableMouse(true)
-		bar:RegisterForDrag("LeftButton")
-		bar:SetScript("OnDragStart", function(self) this:StartMoving() end)
-		bar:SetScript("OnDragStop", function(self) this:StopMovingOrSizing() end)
-
-		bar:SetScript("OnMouseUp", function(button)
-			this:GetParent():Click(button)
-		end)
-
-		bar:SetScript("OnUpdate", function()
-			local curMana, maxMana = DruidManaLib:GetMana()
-			this:SetMinMaxValues(0, 100)
-			this:SetValue(0)
-		end)
-		
-		self.bar = bar
-	end
-
-	self:UpdatePosition()
-
-	self:RegisterEvent("UNIT_DISPLAYPOWER")
-	self:RegisterEvent("PLAYER_AURAS_CHANGED")
-
-	self:UpdatePowerType()
-end
-
-function ClasslessPowerBars:OnDisable()
-	DEFAULT_CHAT_FRAME:AddMessage("ClasslessPowerBars:OnDisable")
-	self.bar:Hide()
-end
-
-function ClasslessPowerBars:UNIT_DISPLAYPOWER()
-	self:UpdatePowerType()
-end
-
-function ClasslessPowerBars:PLAYER_AURAS_CHANGED()
-	if not self.bar:IsShown() then return end
-	self:UpdatePowerType()
-end
-
-function ClasslessPowerBars:IsMoveAbove()
-	return self.db.profile.moveAbove
-end
-
-function ClasslessPowerBars:ToggleMoveAbove()
-	self.db.profile.moveAbove = not self.db.profile.moveAbove
-	self:UpdatePosition()
-end
-
-function ClasslessPowerBars:UpdatePosition()
-	self.bar:ClearAllPoints()
-	if self.db.profile.moveAbove then
-		self.bar:SetPoint("TOPLEFT", 114, -10)
-		self.bar.bd:SetPoint("TOPLEFT", -10, 4)
-		self.bar.bd:SetTexCoord(0.0234375, 0.6875, 0.0, 1.0)
-	else
-		self.bar:SetPoint("BOTTOMLEFT", 114, 23)
-		self.bar.bd:SetPoint("TOPLEFT", -10, 0)
-		self.bar.bd:SetTexCoord(0.0234375, 0.6875, 1.0, 0.0)
+function ClasslessPowerBars:SendDebugMessage(msg)
+    if self.debug then
+		DEFAULT_CHAT_FRAME:AddMessage('|cff0070de[DEBUG:' .. GetTime() .. ']|cffffffff[' .. msg .. ']')
 	end
 end
 
-function ClasslessPowerBars:UpdatePowerType()
-	--if self.loaded and UnitPowerType("player") ~= 0 then
-	--	DEFAULT_CHAT_FRAME:AddMessage("ClasslessPowerBars:UpdatePowerType show bar")
-		self.bar:Show()
-	--else
-	--	self.bar:Hide()
-		self.loaded = true -- so we don't show a mana bar with bogus values
-	--end
+function ClasslessPowerBars:SendServerMessage(msg)
+	SendChatMessage("." .. self.serverMessagePrefix .. " " .. msg)
 end
+
+function ClasslessPowerBars:HandleServerMessage(msg)
+	ClasslessPowerBars:SendDebugMessage("HandleServerMessage "..msg)
+end
+
+ClasslessPowerBars:SetScript("OnEvent", function()
+	if event then
+		ClasslessPowerBars:SendDebugMessage("Event received: "..event)
+		if event == 'ADDON_LOADED' or event == 'PLAYER_ENTERING_WORLD' then
+			ClasslessPowerBars:SendServerMessage("enableAddon")
+		elseif event == 'CHAT_MSG_ADDON' then
+			if string.find(arg1, ClasslessPowerBars.serverMessagePrefix, 1, true) then
+				ClasslessPowerBars:HandleServerMessage(arg2)
+			end
+		end
+	end
+end)
--- src/ClasslessModule.cpp
+++ src/ClasslessModule.cpp
@@ -1,7 +1,22 @@
 #include "ClasslessModule.h"
 
+#include "Entities/Player.h"
+#include "World/World.h"
+
+#ifdef ENABLE_PLAYERBOTS
+#include "playerbot/PlayerbotAI.h"
+#endif
+
 namespace cmangos_module
 {
+    ClasslessPlayerMgr::ClasslessPlayerMgr(Player* inPlayer, ClasslessModule* inModule)
+    : player(inPlayer)
+    , module(inModule)
+    , addonEnabled(false)
+    {
+        
+    }
+
     ClasslessModule::ClasslessModule()
     : Module("Classless", new ClasslessModuleConfig())
     {
@@ -11,5 +26,317 @@ namespace cmangos_module
     const ClasslessModuleConfig* ClasslessModule::GetConfig() const
     {
         return (ClasslessModuleConfig*)Module::GetConfig();
+    }
+
+    void ClasslessModule::OnCharacterCreated(Player* player)
+    {
+        if (GetConfig()->enabled)
+        {
+            if (player)
+            {
+#ifdef ENABLE_PLAYERBOTS
+                // Don't allow bot characters
+                if (!player->isRealPlayer())
+                    return;
+#endif
+                // Create the player manager
+                const uint32 playerId = player->GetObjectGuid().GetCounter();
+                playerMgrs.insert(std::make_pair(playerId, ClasslessPlayerMgr(player, this)));
+            }
+        }
+    }
+
+    void ClasslessModule::OnPreLoadFromDB(Player* player)
+    {
+        if (GetConfig()->enabled)
+        {
+            if (player)
+            {
+#ifdef ENABLE_PLAYERBOTS
+                // Don't allow bot characters
+                if (!player->isRealPlayer())
+                    return;
+#endif
+                // Create the player manager
+                const uint32 playerId = player->GetObjectGuid().GetCounter();
+                playerMgrs.insert(std::make_pair(playerId, ClasslessPlayerMgr(player, this)));
+            }
+        }
+    }
+
+    void ClasslessModule::OnLogOut(Player* player)
+    {
+        if (GetConfig()->enabled)
+        {
+            if (player)
+            {
+                // Delete the player achievement manager
+                const uint32 playerId = player->GetObjectGuid().GetCounter();
+                playerMgrs.erase(playerId);
+            }
+        }
+    }
+
+    void ClasslessModule::OnGetPlayerClassLevelInfo(Player* player, PlayerClassLevelInfo& info)
+    {
+        if (GetConfig()->enabled && player)
+        {
+#ifdef ENABLE_PLAYERBOTS
+            // Don't allow bot characters
+            if (!player->isRealPlayer())
+                return;
+#endif
+
+            // Add a minimum base mana for all classes
+            info.basemana = 100;
+        }
+    }
+
+    void CalculatePowerRegen(Player* player, float& outManaRegen, float& outManaRegenInterrupt)
+    {
+        outManaRegen = 0.0f;
+        outManaRegenInterrupt = 0.0f;
+
+        if (player)
+        {
+            // Mana regen from spirit
+            float powerRegen = player->OCTRegenMPPerSpirit();
+
+            // Simulate mana regen for warriors and rogues
+            if (powerRegen <= 0.0f)
+            {
+                const float spirit = player->GetStat(STAT_SPIRIT);
+                powerRegen = (spirit / 5 + 15) / 2.0f;
+            }
+
+            // Apply PCT bonus from SPELL_AURA_MOD_POWER_REGEN_PERCENT aura on spirit base regen
+            powerRegen *= player->GetTotalAuraMultiplierByMiscValue(SPELL_AURA_MOD_POWER_REGEN_PERCENT, POWER_MANA);
+
+            // Mana regen from SPELL_AURA_MOD_POWER_REGEN aura
+            float powerRegenMP5 = player->GetTotalAuraModifierByMiscValue(SPELL_AURA_MOD_POWER_REGEN, POWER_MANA) / 5.0f;
+
+            // Set regen rate in cast state apply only on spirit based regen
+            int32 modManaRegenInterrupt = player->GetTotalAuraModifier(SPELL_AURA_MOD_MANA_REGEN_INTERRUPT);
+            if (modManaRegenInterrupt > 100)
+            {
+                modManaRegenInterrupt = 100;
+            }
+
+            outManaRegenInterrupt = powerRegenMP5 + powerRegen * modManaRegenInterrupt / 100.0f;
+            outManaRegen = powerRegenMP5 + powerRegen;
+        }
+    }
+
+    void ClasslessModule::OnRegenerate(Player* player, uint8 power, uint32 diff, float& addedValue)
+    {
+        if (GetConfig()->enabled && player)
+        {
+#ifdef ENABLE_PLAYERBOTS
+            // Don't allow bot characters
+            if (!player->isRealPlayer())
+                return false;
+#endif
+            
+            // Simulate mana regen for rogues and warriors
+            const uint8 cls = player->getClass();
+            if (power == POWER_MANA && addedValue == 0.0f && (cls == CLASS_WARRIOR || cls == CLASS_ROGUE))
+            {
+                float modManaRegen = 0.0f;
+                float modManaRegenInterrupt = 0.0f;
+                CalculatePowerRegen(player, modManaRegen, modManaRegenInterrupt);
+
+                const bool recentCast = player->IsUnderLastManaUseEffect();
+                const float ManaIncreaseRate = sWorld.getConfig(CONFIG_FLOAT_RATE_POWER_MANA);
+                if (recentCast)
+                {
+                    addedValue = modManaRegenInterrupt * ManaIncreaseRate * uint32(float(diff) / 1000);
+                }
+                else
+                {
+                    addedValue = modManaRegen * ManaIncreaseRate * uint32(float(diff) / 1000);
+                }
+            }
+
+            // Send updated power to client
+            if (power == POWER_MANA || power == POWER_RAGE || power == POWER_ENERGY)
+            {
+                uint32 curValue = player->GetPower((Powers)(power));
+                uint32 maxValue = player->GetMaxPower((Powers)(power));
+
+                if (power != POWER_RAGE)
+                {
+                    curValue += uint32(addedValue);
+                    if (curValue > maxValue)
+                    {
+                        curValue = maxValue;
+                    }
+                }
+                else
+                {
+                    if (curValue <= uint32(addedValue))
+                    {
+                        curValue = 0;
+                    }
+                    else
+                    {
+                        curValue -= uint32(addedValue);
+                    }
+
+                    curValue /= 10;
+                    maxValue /= 10;
+                }
+
+                PSendAddonMessage(player, "PowerUpdate#%u;%u;%u", power, curValue, maxValue);
+            }
+        }
+    }
+
+    void ClasslessModule::SendAddonMessage(const Player* player, const char* message) const
+    {
+        if (IsAddonEnabled(player))
+        {
+            WorldPacket data;
+
+            std::ostringstream out;
+            out << GetChatCommandPrefix() << "\t" << message;
+
+            char* buf = mangos_strdup(out.str().c_str());
+            char* pos = buf;
+
+            while (char* line = ChatHandler::LineFromMessage(pos))
+            {
+#if EXPANSION == 0
+                ChatHandler::BuildChatPacket(data, CHAT_MSG_ADDON, line, LANG_ADDON);
+#else
+                ChatHandler::BuildChatPacket(data, CHAT_MSG_WHISPER, line, LANG_ADDON);
+#endif
+                player->GetSession()->SendPacket(data);
+            }
+
+            delete[] buf;
+        }
+    }
+
+    void ClasslessModule::PSendAddonMessage(const Player* player, const char* format, ...) const
+    {
+        if (player)
+        {
+            va_list ap;
+            char str[2048];
+            va_start(ap, format);
+            vsnprintf(str, 2048, format, ap);
+            va_end(ap);
+
+            SendAddonMessage(player, str);
+        }
+    }
+
+    std::vector<ModuleChatCommand>* ClasslessModule::GetCommandTable()
+    {
+        static std::vector<ModuleChatCommand> commandTable =
+        {
+            { "enableAddon", std::bind(&ClasslessModule::HandleEnableAddon, this, std::placeholders::_1, std::placeholders::_2), SEC_PLAYER }
+        };
+
+        return &commandTable;
+    }
+
+    bool ClasslessModule::HandleEnableAddon(WorldSession* session, const std::string& args)
+    {
+        if (GetConfig()->enabled)
+        {
+            Player* player = session->GetPlayer();
+            if (player)
+            {
+                if (!IsAddonEnabled(player))
+                {
+                    EnableAddon(player);
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    ClasslessPlayerMgr* ClasslessModule::GetClasslessPlayerMgr(Player* player)
+    {
+        if (GetConfig()->enabled)
+        {
+            ClasslessPlayerMgr* playerMgr = nullptr;
+            if (player)
+            {
+#ifdef ENABLE_PLAYERBOTS
+                // Don't allow bot characters
+                if (!player->isRealPlayer())
+                    return false;
+#endif
+
+                const uint32 playerId = player->GetObjectGuid().GetCounter();
+                auto playerMgrIt = playerMgrs.find(playerId);
+                if (playerMgrIt != playerMgrs.end())
+                {
+                    playerMgr = &playerMgrIt->second;
+                }
+
+                MANGOS_ASSERT(playerMgr);
+                return playerMgr;
+            }
+        }
+
+        return nullptr;
+    }
+
+    const ClasslessPlayerMgr* ClasslessModule::GetClasslessPlayerMgr(const Player* player) const
+    {
+        if (GetConfig()->enabled)
+        {
+            const ClasslessPlayerMgr* playerMgr = nullptr;
+            if (player)
+            {
+#ifdef ENABLE_PLAYERBOTS
+                // Don't allow bot characters
+                if (!player->isRealPlayer())
+                    return false;
+#endif
+
+                const uint32 playerId = player->GetObjectGuid().GetCounter();
+                auto playerMgrIt = playerMgrs.find(playerId);
+                if (playerMgrIt != playerMgrs.end())
+                {
+                    playerMgr = &playerMgrIt->second;
+                }
+
+                MANGOS_ASSERT(playerMgr);
+                return playerMgr;
+            }
+        }
+
+        return nullptr;
+    }
+
+    bool ClasslessModule::IsAddonEnabled(const Player* player) const
+    {
+        if (player)
+        {
+            if (const ClasslessPlayerMgr* playerMgr = GetClasslessPlayerMgr(player))
+            {
+                return playerMgr->IsAddonEnabled();
+            }
+        }
+
+        return false;
+    }
+
+    void ClasslessModule::EnableAddon(Player* player)
+    {
+        if (player)
+        {
+            if (ClasslessPlayerMgr* playerMgr = GetClasslessPlayerMgr(player))
+            {
+                return playerMgr->EnableAddon();
+            }
+        }
     }
 }
